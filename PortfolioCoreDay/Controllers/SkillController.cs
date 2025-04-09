4f01f82314d00bb9503115ddfeb644122d31d58a
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-09 17:51:26 +0300

    Add project files.

---
+++ PortfolioCoreDay/Controllers/SkillController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCoreDay.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult SkillList()
+        {
+            return View();
+        }
+    }
+}
